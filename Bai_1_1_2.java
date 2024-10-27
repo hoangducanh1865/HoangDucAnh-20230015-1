907ea48a3ec541c44146679c83df51fac64c85d5
Hoang Duc Anh <hoangducanh.1865@gmail.com>
2024-10-28 01:38:24 +0700

    Complete assignment 1.1.2

---
+++ Bai_1_1_2.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class Bai_1_1_2 {
+    
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello world! How are you?");
+        System.exit(0);
+    }
+}
