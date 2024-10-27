2ee99177ed501ed0ff8b950062269273f558a880
Hoang Duc Anh <hoangducanh.1865@gmail.com>
2024-10-28 01:37:50 +0700

    Complete assignment 1.1.1

---
+++ Bai_1_1_1.java
@@ -0,0 +1,7 @@
+public class Bai_1_1_1 {
+    
+    public static void main(String[] args) {
+        System.out.println("Xin chao \n cac ban!");
+        System.out.println("Hello \t world!");
+    }
+}
